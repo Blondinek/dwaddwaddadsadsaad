cc62f57f169fa49ab261bd373b4e297594db4d98
Blondinek <58233938+Blondinek@users.noreply.github.com>
2023-03-01 10:35:09 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> AllNotes { get; set; }
+        public string Note;
+
+        public MainPage()
+        {
+            AllNotes = new ObservableCollection<string>();
+            InitializeComponent();
+        }
+        public void SaveNote(object sender, EventArgs e) 
+        {
+            AllNotes.Add(NoteText.Text);
+            NoteText.Text = String.Empty;
+        }
+        public void DelNote(object sender, EventArgs e)
+        {
+            if (NoteList.SelectedItems != null)
+            {
+                foreach (var item in NoteList.SelectedItems)
+                {
+                    AllNotes.Remove((string)item);
+                }
+            }
+            
+        }
+    }
+}
